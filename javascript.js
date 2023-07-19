8bdc0652b65d233b36d35836514149f33964d7cb
Andrea-G05 <andreagiannotti9105@gmail.com>
2023-07-19 16:15:55 +0200

    Fix varibles names and if conditions

--- javascript.js
+++ javascript.js
@@ -1,87 +1,94 @@
 //Randomly return rock, paper or scissor
 function getComputerChoice () {
-    switch( Math.floor( Math.random() * 3 ) ){
+	switch( Math.floor( Math.random() * 3 ) ){
 
-        case 0:
-            return "Rock";
-            break;        
-        
-        case 1:
-            return "Paper";
-            break;
+		case 0:
+			return "Rock";
+			break;        
+		
+		case 1:
+			return "Paper";
+			break;
 
-        case 2:
-            return "Scissor";
-            break;
-    }
+		case 2:
+			return "Scissor";
+			break;
+	}
 }
 
 
 
-function gameRound (playerSelection, computerSelection) {
-    if(playerSelection === "Rock"){
-        switch (computerSelection) {
-            case "Rock":
-                return `Draw! ${playerSelection} against ${computerSelection}.`;
-                break;
-            case "Paper":
-                return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                break;
-            case "Scissor": 
-                return `You Win! ${playerSelection} beats ${computerSelection}.`;
-                break;
-            default:
-                return "Incorrect Inputs";
-                break;
-        }
-    }
+function playRound (playerSelection, computerSelection) {
+	if(playerSelection === "Rock"){
+		switch (computerSelection) {
+			case "Rock":
+				return `Draw! ${playerSelection} against ${computerSelection}.`;
+				break;
+			case "Paper":
+				return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+				break;
+			case "Scissor": 
+				return `You Win! ${playerSelection} beats ${computerSelection}.`;
+				break;
+			default:
+				return "Incorrect Inputs";
+				break;
+		}
+	}
 
-    else if(playerSelection === "Paper"){
-        switch (computerSelection) {
-            case "Paper":
-                return `Draw! ${playerSelection} against ${computerSelection}.`;
-                break;
-            case "Scissor":
-                return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                break;
-            case "Rock": 
-                return `You Win! ${playerSelection} beats ${computerSelection}.`;
-                break;
-            default:
-                return "Incorrect Inputs";
-                break;
-        }
-    }
+	else if(playerSelection === "Paper"){
+		switch (computerSelection) {
+			case "Rock": 
+				return `You Win! ${playerSelection} beats ${computerSelection}.`;
+				break;
+			case "Paper":
+				return `Draw! ${playerSelection} against ${computerSelection}.`;
+				break;
+			case "Scissor":
+				return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+				break;
+			default:
+				return "Incorrect Inputs";
+				break;
+		}
+	}
 
-    else if(playerSelection === "Scissor"){
-        switch (computerSelection) {
-            case "Scissor":
-                return `Draw! ${playerSelection} against ${computerSelection}.`;
-                break;
-            case "Rock":
-                return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-                break;
-            case "Paper": 
-                return `You Win! ${playerSelection} beats ${computerSelection}.`;
-                break;
-            default:
-                return "Incorrect Inputs";
-                break;
-        }
-    }
+	else if(playerSelection === "Scissor"){
+		switch (computerSelection) {
+			case "Rock":
+				return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+				break;
+			case "Paper": 
+				return `You Win! ${playerSelection} beats ${computerSelection}.`;
+				break;
+			case "Scissor":
+				return `Draw! ${playerSelection} against ${computerSelection}.`;
+				break;
+			default:
+				return "Incorrect Inputs";
+				break;
+		}
+	}
 
-    else{
-        return "Incorrect Inputs";
-    }
+	else{
+		return "Incorrect Inputs";
+	}
 }
 
 
+function game(numRounds){
+	for (let i = 0; i < numRounds; i++){
 
+		computerSelection = getComputerChoice();
+		playerSelection = prompt("Choose your move!");
+		playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose your move!");
-playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+		console.log("You chose " + playerSelection)
+		console.log("Computer chose " + computerSelection);
 
-console.log("You chose " + playerSelection)
-console.log("Computer chose " + computerSelection);
-console.log(gameRound(playerSelection, computerSelection));
+		result = playRound(playerSelection, computerSelection);
+		console.log(result);
+ }
+}
+
+game(5);
